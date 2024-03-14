28650a238a51780e138db9353de2ed9f882123e0
canfieca <canfieca@oregonstate.edu>
2024-03-14 16:40:05 -0700

    unit test with JSDOM testing environment to test saveChart function

--- src/lib/chatStorage.test.js
+++ src/lib/chatStorage.test.js
@@ -1,4 +1,44 @@
-// Commemnts 
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs');
+const domTestingLibrary = require('@testing-library/dom');
+require('@testing-library/jest-dom');
+
+const {
+    saveChart,
+    loadAllSavedCharts,
+    loadSavedChart,
+    updateCurrentChartData,
+    loadCurrentChartData
+  } = require('./chartStorage')
 
 
-// Comment
+beforeEach(() => {
+    localStorage.clear()
+})
+
+describe('saveChart', () => {
+    test('saves a chart in the collection of saved charts', () => {
+        const chart = {data: [1, 2, 3]}
+        saveChart(chart)
+        expect(loadAllSavedCharts()).toEqual([chart])
+    })
+
+    test('overwrites the existing data for a chart if an index is specified', () => {
+        const chart1 = {data: [1, 2, 3]}
+        const chart2 = {data: [4, 5, 6]}
+        saveChart(chart1)
+        saveChart(chart2, 0)
+        expect(loadAllSavedCharts()).toEqual([chart2])
+    })
+
+    test('adds a new chart to the end of the array of charts if no index is specified', () => {
+        const chart1 = {data: [1, 2, 3]}
+        const chart2 = {data: [4, 5, 6]}
+        saveChart(chart1)
+        saveChart(chart2)
+        expect(loadAllSavedCharts()).toEqual([chart1, chart2])
+    })
+});

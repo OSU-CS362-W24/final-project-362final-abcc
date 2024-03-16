f57720df41c4b3591d964dcebca21a3d70101bd4
Alex Holly <hollya@oregonstate.edu>
2024-03-15 17:22:15 -0700

    updating cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true,
     baseUrl: "http://localhost:3001"
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,5 @@
 //Simple test
 it('Input is seen in display container', () => {
-  cy.visit('/')
+  cy.visit('/line.html')
+  cy.findByText('Chart title').type('Test Chart')
 })
